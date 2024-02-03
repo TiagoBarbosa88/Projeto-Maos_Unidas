efb8557bf7fe20e8d6be7ece4c8c82a3acdb1573
TiagoBarbosa88 <sombr43@gmail.com>
2024-02-03 10:03:51 -0300

    Componentizando Header e Footer

---
+++ componentes/footer.js
@@ -0,0 +1,46 @@
+const footer = document.querySelector("footer")
+
+footer.innerHTML = ` 
+<div class="rodape limite-largura">
+<div class="rodape-conteudo2">
+  <div class="rodape-logo">
+    <img src="../src/logo-projeto/logo-projeto-social.png" alt="" srcset="">
+  </div>
+  <div class="rodape-info">
+    <ul>
+    <!--   <li>Rua dos Alfeneiros, 04 – Paraty</li>
+      <li>CEP 20200-020 – Rio de Janeiro</li> -->
+      <li>Inscrição municipal: 987654321</li>
+      <li>CNPJ: 12.345.678/9001-23</li>
+    </ul>
+    <p></p>
+  </div>
+</div>
+<div class="rodape-conteudo">
+  <div class="rodape-midias">
+    <h3>Acompanhe nossas Redes Sociais</h3>
+    <ul>
+      <li><a href="#"><img src="../src/icones-projeto/facebook.png" alt=""></a></li>
+      <li><a href="#"><img src="../src/icones-projeto/instagram.png" alt=""></a></li>
+      <li><a href="#"><img src="../src/icones-projeto/you-tube.png" alt=""></a></li>
+    </ul>
+  </div>
+  <div class="rodape-badges">
+    <ul>
+      <li><img src="../src/icones-projeto/selo-homem.png" alt=""></li>
+      <li><img src="../src/icones-projeto/selo-globo-pessoas.png" alt=""></li>
+      <li><img src="../src/icones-projeto/selo-global.png" alt=""></li>
+      <li><img src="../src/icones-projeto/selo-segu.png" alt=""></li>
+    </ul>
+    <ul>
+      <li><img src="../src/icones-projeto/selo-familia.png" alt=""></li>
+      <li><img src="../src/icones-projeto/selo-homem.png" alt=""></li>
+      <li><img src="../src/icones-projeto/selo-prato.png" alt=""></li>
+
+      <li class="badge-quality"><img src="../src/icones-projeto/qualidade.png" alt=""></li>
+    </ul>
+  </div>
+</div>
+
+</div>
+`
