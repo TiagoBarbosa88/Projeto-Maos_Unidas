8f2fb618372d2ec140c54a457418296cff9c1880
TiagoBarbosa88 <sombr43@gmail.com>
2024-02-13 18:53:08 -0300

    feat: Criação do menu hamburguer

--- componentes/header.js
+++ componentes/header.js
@@ -1,27 +1,52 @@
-const header = document.querySelector("header")
+const header = document.querySelector("header");
 
 header.innerHTML = ` 
-<div class="limitar-secao">
-<div>
-  <a href="../index.html">
-    <img id="logo" src="../src/logo-projeto/logo-projeto-social.png" alt="logo mãos unidas">
-  </a>
+<div class="nav-bar">
+  <div class="logo">
+    <a href="../index.html">
+      <img id="logo" class='logo-grande' src="../src/logo-projeto/logo-projeto-social.png" alt="logo mãos unidas">
+      <img id="logo" class='logo-pequeno' src="../src/logo-projeto/logo-maos-unidas-favicon.png" alt="logo mãos unidas">
+    </a>
+  </div>
+
+  <div class="nav-list">
+    <ul class="menu">    
+      <li><a href="../pages/nosso-trabalho.html" class='nav-item'>Nosso Trabalho</a></li>
+      <li><a href="../pages/ranking.html" class='nav-item'>Destaques</a></li>
+      <li><a href="../pages/parceiros.html" class='nav-item'>Parceiros</a></li>  
+      <li><a href="../pages/cadastro.html" class='nav-item'>Cadastro</a></li>
+      <li><a href="../pages/login.html" class="login nav-item">Login</a></li>
+    </ul>        
+  </div>
+  <div class='alterarTema'>
+    <button onclick="alterarTema()" class="modo-claro">
+      <img class="btn-icone" src="../src/icones-projeto/moon.png" alt="icone" />
+    </button>
+  </div>
+  <div class="mobile-menu-icon">
+    <button onclick="menuShow()"><img class="icon" src="../src/svg-menu/burger-menu-svgrepo-com.svg" alt=""></button>
+  </div>
 </div>
 
-<div class="menu-container">
+<div class="mobile-menu">
   <ul class="menu">    
-    <li><a href="../pages/nosso-trabalho.html">Nosso Trabalho</a></li>
-    <li><a href="../pages/ranking.html">Destaques</a></li>
-    <li><a href="../pages/parceiros.html">Parceiros</a></li>  
-    <li><a href="../pages/cadastro.html">Cadastro</a></li>
-    <li><a href="../pages/login.html" class="login">Login</a></li>
-  </ul>        
+    <li><a href="../pages/nosso-trabalho.html" class='nav-item'>Nosso Trabalho</a></li>
+    <li><a href="../pages/ranking.html" class='nav-item'>Destaques</a></li>
+    <li><a href="../pages/parceiros.html" class='nav-item'>Parceiros</a></li>  
+    <li><a href="../pages/cadastro.html" class='nav-item'>Cadastro</a></li>
+    <li><a href="../pages/login.html" class="login nav-item">Login</a></li>
+  </ul> 
 </div>
-<div>
-  <button onclick="alterarTema()" class="modo-claro">
-    <img class="btn-icone" src="../src/icones-projeto/moon.png" alt="icone" />
-  </button>
-</div>
-</div>
-`
+
+`;
+
+function menuShow() {
+  const menuMobile = document.querySelector('.mobile-menu');
+  const icon = document.querySelector('.icon');
+  const isOpen = menuMobile.classList.toggle('open'); // Alternar a classe 'open' e armazenar se foi aberto ou fechado
+
+  // Usar uma operação ternária para definir o src e alt do ícone com base no estado do menu
+  icon.src = isOpen ? "../src/svg-menu/close-square-svgrepo-com.svg" : "../src/svg-menu/burger-menu-svgrepo-com.svg";
+  icon.alt = isOpen ? "Close menu" : "Open menu"; // Define um texto alternativo com base no estado do menu
+}
 
--- js/script.js
+++ js/script.js
@@ -24,12 +24,12 @@ function navegarParaCadastro() {
   window.location.href = "cadastro.html";
 }
 
-function cadastroEmpresas(){
-  window.location.href = 'cadastro-doadores.html'
+function cadastroEmpresas() {
+  window.location.href = 'cadastro-doadores.html';
 }
 
-function cadastroOngs(){
-  window.location.href = 'cadastro-ongs.html'
+function cadastroOngs() {
+  window.location.href = 'cadastro-ongs.html';
 }
 
 
@@ -37,7 +37,7 @@ function cadastroOngs(){
 
 window.scrollSuave = ScrollReveal({ reset: true });
 
-scrollSuave.reveal('.slider', { duration: 1000, delay: 200, distance: '90px'});
+scrollSuave.reveal('.slider', { duration: 1000, delay: 200, distance: '90px' });
 scrollSuave.reveal('.container-card', { duration: 1000, delay: 200 });
 scrollSuave.reveal('.card1', { duration: 500, delay: 500 });
 scrollSuave.reveal('.card2', { duration: 500, delay: 1000 });
@@ -49,7 +49,7 @@ scrollSuave.reveal('.card5', { duration: 500, delay: 3000 });
 
 scrollSuave.reveal('.container-h1', { duration: 1200, delay: 1000 });
 scrollSuave.reveal('.container-video', { duration: 1000, delay: 200 });
-scrollSuave.reveal('.info-left', { origin: 'top', delay: 800});
+scrollSuave.reveal('.info-left', { origin: 'top', delay: 800 });
 scrollSuave.reveal('.info-right', { origin: 'bottom', delay: 800 });
 
 
@@ -64,14 +64,14 @@ scrollSuave.reveal('.lista', { duration: 1000, delay: 200 });
 scrollSuave.reveal('.texto-1', { duration: 1000, delay: 500 });
 scrollSuave.reveal('.texto-2', { duration: 1000, delay: 1500 });
 
-scrollSuave.reveal('.efeito-right', { duration: 1000, delay: 500, origin: 'left', distance: '50px' });
-scrollSuave.reveal('.efeito-left', { duration: 1000, delay: 500, origin: 'right', distance: '50px' });
-scrollSuave.reveal('.efeito-topo', { duration: 1000, delay: 500, origin: 'top', distance: '50px' });
-scrollSuave.reveal('.efeito-bottom', { duration: 1000, delay: 500, origin: 'bottom', distance: '50px' });
+scrollSuave.reveal('.efeito-right', { duration: 1000, delay: 500, origin: 'left', distance: '3.12rem' });
+scrollSuave.reveal('.efeito-left', { duration: 1000, delay: 500, origin: 'right', distance: '3.12rem' });
+scrollSuave.reveal('.efeito-topo', { duration: 1000, delay: 500, origin: 'top', distance: '3.12rem' });
+scrollSuave.reveal('.efeito-bottom', { duration: 1000, delay: 500, origin: 'bottom', distance: '3.12rem' });
 
-scrollSuave.reveal('.step-1', { duration: 1000, delay: 500, distance: '50px'});
-scrollSuave.reveal('.step-2', { duration: 1000, delay: 1000, distance: '50px'});
-scrollSuave.reveal('.step-3', { duration: 1000, delay: 1500, distance: '50px'});
+scrollSuave.reveal('.step-1', { duration: 1000, delay: 500, distance: '3.12rem' });
+scrollSuave.reveal('.step-2', { duration: 1000, delay: 1000, distance: '3.12rem' });
+scrollSuave.reveal('.step-3', { duration: 1000, delay: 1500, distance: '3.12rem' });
 
 
 
